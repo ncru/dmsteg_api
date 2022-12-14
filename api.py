2e4a97341a5d32d88db2e6a615e59e8f073182e6
CodeDFranky <don.franco.ramos@adamson.edu.ph>
2022-12-14 18:54:17 +0800

    for nic

--- api.py
+++ api.py
@@ -5,13 +5,12 @@ from modules.threesys import *
 from flask import Flask, render_template, request, send_file, url_for, flash, redirect
 from werkzeug.utils import secure_filename
 import json
-import base64
 
 INSERT_PDF_RETURN_ROW = (
     "INSERT INTO threesyspdfs (pdf_metadata, pdf_data) VALUES (%s, %s) RETURNING *;"
 )
 
-# load_dotenv()
+load_dotenv()
 
 app = Flask(__name__)
 url = os.getenv("DATABASE_URL")
@@ -44,11 +43,10 @@ def main():
     return render_template("index.html")
 
 
-# FileStorage format
-
-
 @app.route("/generate", methods=["POST"])
 def generate():
+    # os.remove('file_path')
+
     if request.method == "POST":
         file = request.files["file"]
         file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
@@ -65,61 +63,46 @@ def generate():
 
         rpdf_data = bytes(rpdf_data)
         with open(
-            os.path.join(app.config["UPLOAD_FOLDER"], f"temp-{rpdf_id}.pdf"), "wb"
+            os.path.join(app.config["UPLOAD_FOLDER"], f"temp-{rpdf_id}-modified.pdf"),
+            "wb",
         ) as _f:
             _f.write(rpdf_data)
 
-        # return {
-        #     "pdf_id": rpdf_id,
-        #     "message": "VERIFIED PDF SUCCESSFULLY CREATED",
-        #     "metadata": rpdf_metadata,
-        # }, 201
+        return {
+            "pdf_id": rpdf_id,
+            "message": "Verified pdf successfully created",
+            "metadata": rpdf_metadata,
+        }, 201
 
-        images = grab_first_page_images(document)
-        dms = grab_all_dms(images)
+        # images = grab_first_page_images(document)
+        # dms = grab_all_dms(images)
 
-        #  INSERT THERE MAY ONLY BE ONE 3SYS STEG
+        # #  INSERT THERE MAY ONLY BE ONE 3SYS STEG
 
-        # if dms in the pdf check if they are 3sys
-        if len(dms) > 0:
-            valid_dm = check_dms_for_steganography(dms)
-            if valid_dm != False:
-                reg_msg = read_dm(valid_dm)
-                steg_msg = read_steganography(valid_dm)
-                return "THE DOCUMENT IS ALREADY SIGNED", 400
-                # print("MESSAGE:", reg_msg)
-                # print("SECRET:", steg_msg)
-            else:
-                return "THE DOCUMENT MAY NOT BE VALID", 400
-                # print("THE DOCUMENT MAY NOT BE VALID")
-        # if there are no dms then check if the margins are clear
-        elif margins_passed(document):
-            ord_dm = generate_dm(document)
-            steg_dm = steganography(ord_dm)
-            # put dm in pdf
-            modified_document = put_steg_dm_in_pdf(document, steg_dm)
-            base_name = os.path.basename(modified_document.name)
-            new_name = base_name[: base_name.find(".pdf")] + "-modified.pdf"
-            modified_document.save(os.path.join(app.config["UPLOAD_FOLDER"], new_name))
-            return "DM STEG ADDED TO DOCUMENT", 200
-        else:
-            return "THE DOCUMENT MUST HAVE CLEAR 1 INCH MARGINS", 400
-
-    # if request.method == "POST":
-    #     f = request.files["file"]
-    #     f.save(os.path.join(app.config["UPLOAD_FOLDER"], f.filename))
-
-    #     # get file, text to be hidden here for processing
-    #     file = "buffer/%s" % (f.filename)
-    #     text = request.form["text"]
-    #     #
-    #     # - external function for generating DM STEG here -
-    #     #
-    #     dmsteg_gen.getfile(file)  # external function for inserting DM STEG
-
-    #     # return file
-    #     return send_file(file, as_attachment=True)  # ENDPOINT RESPONSE
-    #     # return render_template("success.html", name=text) # to test whether text is read from form
+        # # if dms in the pdf check if they are 3sys
+        # if len(dms) > 0:
+        #     valid_dm = check_dms_for_steganography(dms)
+        #     if valid_dm != False:
+        #         reg_msg = read_dm(valid_dm)
+        #         steg_msg = read_steganography(valid_dm)
+        #         return "THE DOCUMENT IS ALREADY SIGNED", 400
+        #         # print("MESSAGE:", reg_msg)
+        #         # print("SECRET:", steg_msg)
+        #     else:
+        #         return "THE DOCUMENT MAY NOT BE VALID", 400
+        #         # print("THE DOCUMENT MAY NOT BE VALID")
+        # # if there are no dms then check if the margins are clear
+        # elif margins_passed(document):
+        #     ord_dm = generate_dm(document)
+        #     steg_dm = steganography(ord_dm)
+        #     # put dm in pdf
+        #     modified_document = put_steg_dm_in_pdf(document, steg_dm)
+        #     base_name = os.path.basename(modified_document.name)
+        #     new_name = base_name[: base_name.find(".pdf")] + "-modified.pdf"
+        #     modified_document.save(os.path.join(app.config["UPLOAD_FOLDER"], new_name))
+        #     return "DM STEG ADDED TO DOCUMENT", 200
+        # else:
+        #     return "THE DOCUMENT MUST HAVE CLEAR 1 INCH MARGINS", 400
 
 
 @app.route("/verify", methods=["POST"])
